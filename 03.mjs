b5ac0fba39a3c0448de7df60a53e9e950f20645c
Ivett LATÁK <ivett.latak@shiwaforce.com>
2023-12-03 12:08:02 +0100

    Day 3 - part 2 - gears found, but not answering the question :D

--- 03.mjs
+++ 03.mjs
@@ -1,29 +1,31 @@
 import { readFile } from "./utils.mjs";
 
-const separator = ".";
-const snowflakes = ["*", "+", "#", "$", "&", "@", "/", "=", "%"];
+const SEPARATOR = ".";
+const GEAR_SYMBOL = "*";
 
-const engine = readFile("./input_03");
+const engine = readFile("./input_03_ex_1");
 
-const mappedEngine = getMappedEngine(engine);
+const engineParts = getEngineParts(engine);
 
-console.log(mappedEngine);
+console.log(engineParts);
 
-function getMappedEngine(lines) {
-	const isolatedNumbers = [];
+function getEngineParts(lines) {
+	let gears = {};
 	let enginePartNumberSum = 0;
 	lines.forEach((line, lineIndex) => {
 		let i = 0;
 		while (i < line.length) {
 			const curChar = line.at(i);
-			if (curChar !== separator && !isNaN(curChar)) {
+			if (curChar !== SEPARATOR && !isNaN(curChar)) {
 				const [foundNumber, numberLength] = getFirstNumberAsString(line.substring(i));
-				console.log(foundNumber);
 				const isFoundNumberEnginePart = getHasNeighbour(lines, lineIndex, i, i + numberLength - 1);
 				if (isFoundNumberEnginePart) {
 					enginePartNumberSum += Number(foundNumber);
-				} else {
-					isolatedNumbers.push(foundNumber);
+					
+					const adjacentToGearSymbols = getIsAdjacentToGear(lines, lineIndex, i, i + numberLength - 1);
+					if (adjacentToGearSymbols.hasAdjacentGear) {
+						gears = mergeFlakeIndices(gears, adjacentToGearSymbols.adjacentFlakeIndices);
+					}
 				}
 				i += numberLength;
 			} else {
@@ -31,8 +33,9 @@ function getMappedEngine(lines) {
 			}
 		}
 	});
-	console.log(isolatedNumbers);
-	return enginePartNumberSum;
+	
+	const exactGearsCount = Object.values(gears).filter((gearCount) => gearCount === 2).length;
+	return {enginePartNumberSum, exactGearsCount};
 }
 
 function getFirstNumberAsString(word) {
@@ -45,17 +48,68 @@ function getFirstNumberAsString(word) {
 	return [foundNumber, i];
 }
 
-function hasSnowFlake(word) {
+function getSymbols(word, onlyAllowedSymbol, forbiddenSymbol = SEPARATOR) {
 	if (!word) {
 		return false;
 	}
-	return word.split('').reduce((acc, cur) =>  acc || isNaN(cur) && cur !== separator, false);
+	return word.split('').reduce((acc, cur, index) => {
+		const hasSymbol = cur === onlyAllowedSymbol || !onlyAllowedSymbol && isNaN(cur) && cur !== forbiddenSymbol;
+		return {
+			hasSymbol: acc.hasSymbol || hasSymbol,
+			symbolIndices: [ ...acc.symbolIndices, ...(hasSymbol ? [index] : []) ]
+		}
+	}, {hasSymbol: false, symbolIndices: []});
 }
 
 function getHasNeighbour(lines, lineIndex, startIndex, endIndex) {
-	const hasFlakeBefore = startIndex === 0 ? false : hasSnowFlake(lines[lineIndex].at(startIndex - 1));
-	const hasFlakeAfter = endIndex >= lines[lineIndex].length ? false : hasSnowFlake(lines[lineIndex].at(endIndex + 1));
-	const hasFlakeBeforeLine = lineIndex === 0 ? false : hasSnowFlake(lines[lineIndex - 1].slice((startIndex === 0 ? startIndex : startIndex - 1), (endIndex >= (lines[lineIndex - 1].length - 1) ? (endIndex + 1) : endIndex + 2)));
-	const hasFlakeAfterLine = lineIndex >= (lines.length - 1) ? false : hasSnowFlake(lines[lineIndex + 1].slice((startIndex === 0 ? startIndex : startIndex - 1), (endIndex >= (lines[lineIndex + 1].length - 1) ? (endIndex + 1) : endIndex + 2)));
+	const hasFlakeBefore = startIndex === 0 ? false : getSymbols(lines[lineIndex].at(startIndex - 1)).hasSymbol;
+	const hasFlakeAfter = endIndex >= lines[lineIndex].length ? false : getSymbols(lines[lineIndex].at(endIndex + 1)).hasSymbol;
+	const hasFlakeBeforeLine = lineIndex === 0 ? false : getSymbols(lines[lineIndex - 1].slice((startIndex === 0 ? startIndex : startIndex - 1), (endIndex >= (lines[lineIndex - 1].length - 1) ? (endIndex + 1) : endIndex + 2))).hasSymbol;
+	const hasFlakeAfterLine = lineIndex >= (lines.length - 1) ? false : getSymbols(lines[lineIndex + 1].slice((startIndex === 0 ? startIndex : startIndex - 1), (endIndex >= (lines[lineIndex + 1].length - 1) ? (endIndex + 1) : endIndex + 2))).hasSymbol;
 	return hasFlakeBefore || hasFlakeAfter || hasFlakeBeforeLine || hasFlakeAfterLine;
+}
+
+function getIsAdjacentToGear(lines, lineIndex, startIndex, endIndex) {
+	let adjacentFlakeIndices = {};
+	const symbolBefore = startIndex === 0 ? {} : getSymbols(lines[lineIndex].at(startIndex - 1), GEAR_SYMBOL);
+	if (symbolBefore.hasSymbol) {
+		const gearCount = adjacentFlakeIndices[getGearId(lineIndex, startIndex - 1)];
+		adjacentFlakeIndices[getGearId(lineIndex, startIndex - 1)] = gearCount ? gearCount + 1 : 1;
+	}
+	const symbolAfter = endIndex >= lines[lineIndex].length ? {} : getSymbols(lines[lineIndex].at(endIndex + 1), GEAR_SYMBOL);
+	if (symbolAfter.hasSymbol) {
+		const gearCount = adjacentFlakeIndices[getGearId(lineIndex, endIndex + 1)];
+		adjacentFlakeIndices[getGearId(lineIndex, endIndex + 1)] = gearCount ? gearCount + 1 : 1;
+	}
+	const symbolBeforeLine = lineIndex === 0 ? false : getSymbols(lines[lineIndex - 1].slice((startIndex === 0 ? startIndex : startIndex - 1), (endIndex >= (lines[lineIndex - 1].length - 1) ? (endIndex + 1) : endIndex + 2)), GEAR_SYMBOL);
+	if (symbolBeforeLine.hasSymbol) {
+		symbolBeforeLine.symbolIndices.forEach(
+			(symbolIndex) => {
+				const gearCount = adjacentFlakeIndices[getGearId(lineIndex - 1, startIndex === 0 ? startIndex + symbolIndex : startIndex - 1 + symbolIndex)];
+				adjacentFlakeIndices[getGearId(lineIndex - 1, startIndex === 0 ? startIndex + symbolIndex : startIndex - 1 + symbolIndex)] = gearCount ? gearCount + 1 : 1;
+			}
+		);
+	}
+	const symbolAfterLine = lineIndex >= (lines.length - 1) ? false : getSymbols(lines[lineIndex + 1].slice((startIndex === 0 ? startIndex : startIndex - 1), (endIndex >= (lines[lineIndex + 1].length - 1) ? (endIndex + 1) : endIndex + 2)), GEAR_SYMBOL);
+	if (symbolAfterLine.hasSymbol) {
+		symbolAfterLine.symbolIndices.forEach(
+			(symbolIndex) => {
+				const gearCount = adjacentFlakeIndices[getGearId(lineIndex + 1, startIndex === 0 ? startIndex + symbolIndex : startIndex - 1 + symbolIndex)];
+				adjacentFlakeIndices[getGearId(lineIndex + 1, startIndex === 0 ? startIndex + symbolIndex : startIndex - 1 + symbolIndex)] = gearCount ? gearCount + 1 : 1;
+			}
+		);
+	}
+	return { hasAdjacentGear: symbolBefore.hasSymbol || symbolAfter.hasSymbol || symbolBeforeLine.hasSymbol || symbolAfterLine.hasSymbol, adjacentFlakeIndices};
+}
+
+function getGearId(rowIndex, columnIndex) {
+	return `${rowIndex}_${columnIndex}`;
+}
+
+function mergeFlakeIndices(flakeIndices1, flakeIndices2) {
+	Object.keys(flakeIndices2).forEach((flakeKey) => {
+		const flakeCount = flakeIndices1[flakeKey];
+		flakeIndices1[flakeKey] = flakeCount ? flakeCount + flakeIndices2[flakeKey] : flakeIndices2[flakeKey];
+	});
+	return flakeIndices1;
 }
