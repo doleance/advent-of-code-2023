65195ff95ce4a59e5724dfa58eb6140ed0cfda24
Ivett LATÁK <ivett.latak@shiwaforce.com>
2023-12-05 00:41:26 +0100

    Day 4 - part 2

--- 04.mjs
+++ 04.mjs
@@ -5,22 +5,50 @@ const GEAR_SYMBOL = "*";
 
 const cards = readFile("./input_04");
 
-const scoreOfCards = getScores(cards);
+const [scoreOfCardsWithPrivateRules, totalScratchCards] = getScores(cards);
 
-console.log(scoreOfCards);
+console.log({scoreOfCardsWithPrivateRules, totalScratchCards});
 
 function getScores(cards) {
 	let score = 0;
+	const cardWins = [];
 	cards.forEach((card) => {
 		const [,drawn,chosen] = card.split(/[:|]/);
 		const drawnNumbers = drawn.split(' ').filter((item) => item).map((item) => Number(item));
 		const chosenNumbers = chosen.split(' ').filter((item) => item).map((item) => Number(item));
-		score += getScoreByCard(drawnNumbers, chosenNumbers);
+		const winningNumbersCount = getWinningNumbersCount(drawnNumbers, chosenNumbers);
+		score += winningNumbersCount ? Math.pow(2, winningNumbersCount - 1) : 0;
+		cardWins.push(winningNumbersCount);
 	});
-	return score;
+	const winningCardCount = countWinningCardCounts(cardWins).reduce((acc, cur) => acc + cur, 0);
+	
+	return [score, winningCardCount];
 }
 
-function getScoreByCard(drawnNumbers, chosenNumbers) {
+function getWinningNumbersCount(drawnNumbers, chosenNumbers) {
 	const winningNumbers = chosenNumbers.filter((chosen) => drawnNumbers.includes(chosen));
-	return winningNumbers.length ? Math.pow(2, winningNumbers.length - 1) : 0;
+	return winningNumbers.length;
+}
+
+// TODO: somehow make it work
+
+/* function countWins(cardWins, currentIndex) {
+	const currentWinCount = cardWins[currentIndex];
+	if (currentWinCount === 0) {
+		return 1;
+	}
+	const winnedCopies = cardWins.slice(currentIndex + 1, Math.min(currentIndex + currentWinCount + 1, cardWins.length));
+	return winnedCopies.reduce((acc, cur, index) => {
+		return acc + 1 + cur * countWins(cardWins, currentIndex + index + 1);
+	}, 0);
+} */
+
+function countWinningCardCounts(cardWins) {
+	const winningCardCounts = Array(cardWins.length).fill(1);
+	for (let i = 0; i < cardWins.length; i++) {
+		for (let j = 0; j < cardWins[i]; j++) {
+			winningCardCounts[i + 1 + j] += winningCardCounts[i];
+		}
+	}
+	return winningCardCounts;
 }
